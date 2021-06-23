67942cc4ee1a502c32567af620b532cff85becfc
Aayam Shrestha <63101665+Aayam555@users.noreply.github.com>
2021-06-23 18:27:09 +0545

    Add files via upload

---
+++ Account Notes Website/pages/chapter.js
@@ -0,0 +1,11 @@
+const displayImage = (chapter, pages) => {
+  let imageHTMLTemplate;
+  for (let pageNumber = 1; pageNumber <= pages; pageNumber++){
+    imageHTMLTemplate += `
+        <a target="_blank" href="../../Account Notes/Chapter ${chapter}/page ${pageNumber}.png">
+          <img src="../../Account Notes/Chapter ${chapter}/page ${pageNumber}.png">
+        </a> <br>
+      `;
+  }
+  document.getElementById("images").innerHTML = imageHTMLTemplate.replace("undefined", "");
+}
