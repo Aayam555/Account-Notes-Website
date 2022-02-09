88bd546a589a34aa42400c75e06cb7db2910e4ed
Aayam555 <63101665+Aayam555@users.noreply.github.com>
2022-02-09 19:41:50 +0545

    Chapter 13 added

--- pages/chapter.js
+++ pages/chapter.js
@@ -2,8 +2,8 @@ const displayImage = (chapter, pages) => {
   let imageHTMLTemplate;
   for (let pageNumber = 1; pageNumber <= pages; pageNumber++){
     imageHTMLTemplate += `
-        <a target="_blank" href="../../Account Notes/Chapter ${chapter}/page ${pageNumber}.png">
-          <img src="../../Account Notes/Chapter ${chapter}/page ${pageNumber}.png">
+        <a target="_blank" href="../Account Notes/Chapter ${chapter}/page ${pageNumber}.png">
+          <img src="../Account Notes/Chapter ${chapter}/page ${pageNumber}.png">
         </a> <br>
       `;
   }
